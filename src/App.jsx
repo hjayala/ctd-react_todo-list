ce4e95cda26307ea463da4ddf5f3727c5e21c4d4
Hector Ayala <hjayala70@gmail.com>
2026-05-10 16:44:22 -0400

    Lesson 04 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,19 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import { useState } from 'react'
 
-const todos = [
-    {id:1, title: "Complete Scrimba React course"},
-    {id:2, title: "Take notes on Discrete Math"},
-    {id:3, title: "Review Data Structures"},
-]
-
 function App() {
-  // The underscore here lets React now setTodoList is left unused intentionally
-  // I had to add it, since it kept giving me an error until then
-  const [todoList, _setTodoList] = useState(todos)
+  const [todoList, setTodoList] = useState([])
+
+  function addTodo(todoTitle) {
+    const newTodo = { id: Date.now(), title: todoTitle }
+    setTodoList(previous => [newTodo, ...previous])
+  }
 
   return (
     <>
       <div>
         <h1>Hector Ayala's Todos</h1>
-        <TodoForm />
+        <TodoForm onAddTodo={addTodo} />
         <TodoList todoList={todoList} />
       </div>
     </>
